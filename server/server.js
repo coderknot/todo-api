f4884aed96aa2d21e8b23851f4bd7052e5510c96
Chris Carr <coder.knot@gmail.com>
2017-09-12 23:09:11 -0700

    add DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -34,12 +34,12 @@ app.get('/todos', (req, res) => {
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
-  if(!ObjectID.isValid(id)) {
+  if (!ObjectID.isValid(id)) {
     return res.status(404).send()
   }
 
   Todo.findById(id).then((todo) => {
-    if(!todo) {
+    if (!todo) {
       return res.status(404).send();
     }
     res.send({
@@ -49,6 +49,23 @@ app.get('/todos/:id', (req, res) => {
   }).catch((e) => {
     res.status(400).send();
   });
+});
+
+app.delete('/todos/:id', (req, res) => {
+ var id = req.params.id;
+
+ if (!ObjectID.isValid(id)) {
+   return res.status(404).send();
+ }
+
+ Todo.findByIdAndRemove(id).then((todo) => {
+   if (!todo) {
+     return res.status(404).send();
+   }
+   res.send({todo});
+ }).catch((e) => {
+   res.status(400).send();
+ });
 });
 
 app.listen(port, () => {
